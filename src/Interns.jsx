11198ccc7687c31ed011df386b8edb9dc4abbf2d
Arome Jonathan Ukpoju <99257453+Tnath1@users.noreply.github.com>
2024-03-26 08:28:31 +0100

    cleaned my code

--- src/Interns.jsx
+++ src/Interns.jsx
@@ -4,47 +4,20 @@ import DataContext from "./DataContext";
 const Interns = () => {
   const { items } = useContext(DataContext);
 
-  // const ObjectCard = () => (
-  //            <div className="card">
-  //              <img src={items.imgSrc} alt={object.name} />
-  //              <h3>{items.name}</h3>
-  //              <p>Score: {items.score}/100</p>
-  //              <p>{items.about}</p>
-  //            </div>
-  //          );
 
   return (
-    // <div>
-    //   <h2>Items List</h2>
-
-    //     <div className="container">
-    //       <div className="card-container">
-    //         <div className="card">
-    //         {items.map((item) => (
-    //           <div key={item.id}>
-    //             <img src={item.imgSrc}  alt={item.name} />
-    //             <h3>{item.name}</h3>
-    //             <p>{item.score}</p>
-    //           </div>
-    //         ))}
-    //         </div>
-    //       </div>
-    //     </div>
-
-    // </div>
 
     <div>
       <h2>Items List</h2>
       <div className="container">
         <div className="card-container">
           {items.map((item) => (
-            // Move the "card" class here, so each item is its own card
             <div className="card" key={item.id}>
               <img src={item.imgSrc} alt={item.name} />
               <div className="text">
-              <h3>{item.name}</h3>
-              <p>Track: {item.about}</p>
-              <p>Score: {item.score}</p>
+                <h3>{item.name}</h3>
+                <p>Track: {item.about}</p>
+                <p>Score: {item.score}</p>
               </div>
             </div>
           ))}
@@ -52,50 +25,7 @@ const Interns = () => {
       </div>
     </div>
   );
-
-  // return (
-  //   <div className="app">
-  //     <div className="card-container">
-  //       {sortedObjects.map(object => (
-  //         <ObjectCard key={object.id} object={object} />
-  //       ))}
-  //     </div>
-  //   </div>
-  // );
 };
 
 export default Interns;
 
-// import React, { useData } from 'react'
-
-// const Interns = () => {
-//   const { items } = useData();
-
-//       const ObjectCard = () => (
-//         <div className="card">
-//           <img src={items.imgSrc} alt={object.name} />
-//           <h3>{items.name}</h3>
-//           <p>Score: {items.score}/100</p>
-//           <p>{items.about}</p>
-//         </div>
-//       );
-
-// return (
-//   <div className="app">
-//     <div className="card-container">
-//       {sortedObjects.map(object => (
-//         <ObjectCard key={object.id} object={object} />
-//       ))}
-//     </div>
-//   </div>
-// );
-// }
-
-// // {items.map((item) => (
-// //   <div key={item.id}>
-// //     <h3>{item.name}</h3>
-// //     <p>Score: {item.score}</p>
-// //   </div>
-// // ))}
-
-// export default Interns
--- src/Nav.jsx
+++ src/Nav.jsx
@@ -19,23 +19,3 @@ const Nav = () => {
 
 export default Nav;
 
-
-// const Nav = () => {
-//   // State to manage the checkbox checked status
-//   const [isChecked, setIsChecked] = useState(false);
-
-//   // Function to toggle the state
-//   const toggleChecked = () => setIsChecked(!isChecked);
-
-//   return (
-//     <div>
-//       <h3>All interns</h3>
-//       <label className="switch">
-//         <input type="checkbox" checked={isChecked} onChange={toggleChecked} />
-//         <span className="slider round"></span>
-//       </label>
-//     </div>
-//   );
-// };
-
-// export default Nav;
